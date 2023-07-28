b02246cd9abe4db0d9eb3e0266552ebb5fb83f23
Santiago Henrique <105302316+San2855@users.noreply.github.com>
2023-07-28 20:18:38 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ const listaLocalizacoes = [
   {'name': 'Praça de eventos', 'latitude': -8.3282630931819, 'longitude': -36.420677011490824},
   {'name': 'Maria Cristina', 'latitude': -8.322165115376684, 'longitude': -36.4131265747507},
   {'name': 'Hotel Lux', 'latitude': -8.342648493218235, 'longitude': -36.417996839356334},
-  {'name': 'Entrada da Cohab 1', 'latitude': -8.349024814316115, 'longitude': -36.40816293077253},
+  {'name': 'Entrada da Cohab 1', 'latitude': 8.3486585, 'longitude': -36.4093192},
   {'name': 'Escola Dr. Sebatião Cabral', 'latitude': -8.342163205471854, 'longitude': -36.416830941150298},
   {'name': 'Colégio Éxito', 'latitude': -8.325263839326702, 'longitude': -36.4186024306167},
   {'name': 'Escola Prof. Donino', 'latitude': -8.333162206111101, 'longitude': -36.417373357004664},
@@ -16,7 +16,7 @@ const listaLocalizacoes = [
   {'name': 'Erem João Monteiro', 'latitude': -8.339019961440135, 'longitude': -36.43257982213477},
   {'name': 'Câmara municipal', 'latitude': -8.332738694916673, 'longitude': -36.41868088328961},
   {'name': 'Posto Petrovia', 'latitude': -8.337249932009748, 'longitude': -36.4303272889118},
-  {'name': 'Trevo de acesso', 'latitude': -8.345068172150173, 'longitude': -36.43325160699831},
+  {'name': 'Trevo de acesso', 'latitude': -8.345068172150173, 'longitude': -36.43325160699831}
 ];
 
 // Calculo da distancia
@@ -36,44 +36,36 @@ function calcularDistancia(latitude1, longitude1, latitude2, longitude2) {
 function toRadians(graus) {
   return graus * (Math.PI / 180);
 }
+
 // Calculo do raio de tolerancia
 function verificarProximidade(latitude, longitude) {
-  const localizacaoExata = listaLocalizacoes.find(localizacao => localizacao.latitude === latitude && localizacao.longitude === longitude);
+  const localizacoesProximas = [];
 
-  if (localizacaoExata) {
-    document.getElementById("localizacoes").textContent = `Você está no/a ${localizacaoExata.name}.`;
-  } else {
-    const localizacoesProximas = [];
-    const distanciaTolerancia = 0.01; // 10 metros (0.01 km)
-
-    listaLocalizacoes.forEach(localizacao => {
-      const distancia = calcularDistancia(latitude, longitude, localizacao.latitude, localizacao.longitude);
-      if (distancia < distanciaTolerancia) {
-        localizacoesProximas.push({ name: localizacao.name, distancia: distancia.toFixed(2) });
-      }
-    });
-
-    if (localizacoesProximas.length > 0) {
-      let localizacoesHTML = "";
-      localizacoesProximas.forEach(localizacao => {
-        if (localizacao.distancia === '0.00') {
-          localizacoesHTML += `<p>Você está no/a ${localizacao.name}.\n</p>`;
-        } else {
-          localizacoesHTML += `<p>Você está a ${localizacao.distancia} km de ${localizacao.name}\n</p>`;
-        }
-      });
-      document.getElementById("localizacoes").innerHTML = localizacoesHTML;
-    } else {
-      document.getElementById("localizacoes").textContent = "Você não está próximo de nenhuma localização.";
+  listaLocalizacoes.forEach(localizacao => {
+    const distancia = calcularDistancia(latitude, longitude, localizacao.latitude, localizacao.longitude);
+    if (distancia < 0.1) { // 100 metros (0.1 km)
+      localizacoesProximas.push({ name: localizacao.name, distancia: distancia.toFixed(2) });
     }
+    
+  });
 
-    enviarDistanciaTelegram(localizacoesProximas); // Sempre envie a mensagem para o Telegram
+  if (localizacoesProximas.length > 0) {
+    let localizacoesHTML = "";
+    localizacoesProximas.forEach(localizacao => {
+      localizacoesHTML += `<p>Sua localização atual é: ${localizacao.name}\n</p>`;
+    });
+    document.getElementById("localizacoes").innerHTML = localizacoesHTML;
+    enviarDistanciaTelegram(localizacoesProximas);
+  } 
+  else {
+    document.getElementById("localizacoes").textContent = "Você não está próximo de nenhuma localização.";
+    enviarDistanciaTelegram(localizacoesProximas);
   }
 }
-// Enviar mensagem para o telegram
+ // Enviar mensagem para o telegram
 function enviarDistanciaTelegram(localizacoes) {
   const url = '/enviar_distancia';
-  const chatId = -1001740152545;
+  const chatId = -1001740152545; 
   const data = { localizacoes: localizacoes, chat_id: chatId };
 
   fetch(url, {
@@ -100,7 +92,7 @@ function obterDistancia(latitude1, longitude1) {
 
   listaLocalizacoes.forEach(localizacao => {
     const distancia = calcularDistancia(latitude1, longitude1, localizacao.latitude, localizacao.longitude);
-    mensagem += `Você está a ${localizacao.distancia} km de ${localizacao.name}\n`;
+    mensagem += `Sua localização atual é: ${localizacao.name}\n`;
   });
 
   return mensagem;
@@ -122,8 +114,7 @@ function obterLocalizacao() {
   }
 }
 
-setInterval(obterLocalizacao, 300000); // Chama a função a cada 5 minutos (300.000 milissegundos)
+setInterval(obterLocalizacao, 5000); // Chama a função a cada 5 segundos
 
 const btnObterLocalizacao = document.getElementById("btnObterLocalizacao");
 btnObterLocalizacao.addEventListener("click", obterLocalizacao);
-
